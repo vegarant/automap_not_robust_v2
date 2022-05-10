565be3b1854184789f911c776fda43e0c90c66fc
Vegard Antun <vegarant@student.matnat.uio.no>
2022-05-10 14:07:43 +0200

    Edited README.md

--- adv_tools_PNAS/automap_config.py
+++ adv_tools_PNAS/automap_config.py
@@ -1,9 +1,9 @@
 # Update the paths so that they match the underlying data.
 
-src_weights        = '/mn/sarpanitu/ansatte-u4/vegarant/storage_matters_arising_final';
-src_data           = '/mn/sarpanitu/ansatte-u4/vegarant/storage_matters_arising_final';
-src_k_mask         = '/mn/sarpanitu/ansatte-u4/vegarant/storage_matters_arising_final';
-src_automap_runner = '/mn/sarpanitu/ansatte-u4/vegarant/storage_matters_arising_final/runners';
+src_weights        = '/mn/kadingir/vegardantun_000000/nobackup/storage_all/storage_automap_not_robust' 
+src_data           = '/mn/kadingir/vegardantun_000000/nobackup/storage_all/storage_automap_not_robust'
+src_k_mask         = '/mn/kadingir/vegardantun_000000/nobackup/storage_all/storage_automap_not_robust' 
+src_automap_runner = '/mn/kadingir/vegardantun_000000/nobackup/storage_all/storage_automap_not_robust/runners' 
 
 
 
